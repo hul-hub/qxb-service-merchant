385598751a4f53d31b22818ec45447aa2128298f
sunkang <qzsk0921@hotmail.com>
2020-11-25 09:53:41 +0800

    商户新增-开户信息

--- src/views/merchant/add/Account.vue
+++ src/views/merchant/add/Account.vue
@@ -14,17 +14,71 @@
         @click="showPicker = true"
         :rules="[{ required: true, message: '请选择账户类型' }]"
       />
-
-      <!-- <upload
-        @childUpload="idfrontimgFun"
-        v-model="accountItem.accountfrontimg"
-        label="身份证正面"
-      ></upload>
+      <van-field
+        readonly
+        clickable
+        label="开户省份"
+        :value="accountItem.provinceP"
+        @click="showPickerProvince = true"
+        :rules="[{ required: true, message: '请选择开户省份' }]"
+      />
+      <van-field
+        readonly
+        clickable
+        label="开户城市"
+        :value="accountItem.cityP"
+        @click="showPickerCity = true"
+        :rules="[{ required: true, message: '请选择开户城市' }]"
+      />
+      <van-field
+        readonly
+        clickable
+        label="开户银行"
+        :value="accountItem.bankidP"
+        @click="showPickerBank = true"
+        :rules="[{ required: true, message: '请选择开户银行' }]"
+      />
+      <van-field
+        readonly
+        clickable
+        label="开户银行支行"
+        :value="accountItem.branchidP"
+        @click="showPickerBranch = true"
+        :rules="[{ required: true, message: '请选择开户银行支行' }]"
+      />
+      <van-field
+        readonly
+        clickable
+        label="证件类型"
+        :value="accountItem.idcardtypeP"
+        @click="showPickerIdcardtype = true"
+        :rules="[{ required: true, message: '请选择证件类型' }]"
+      />
+      <van-field
+        v-model="accountItem.idcard"
+        label="证件号码"
+        :rules="[{ required: true, message: '请填证件号码' }]"
+      />
+      <van-field
+        v-model="accountItem.address"
+        label="持卡人地址"
+        :rules="[{ required: true, message: '请填持卡人地址' }]"
+      />
+      <van-field
+        v-model="accountItem.tel"
+        label="银行预留手机号"
+        :rules="[{ required: true, message: '请填银行预留手机号' }]"
+      />
+      <van-field
+        v-model="accountItem.accountcode"
+        label="银行账号"
+        :rules="[{ required: true, message: '请填银行账号' }]"
+      />
       <upload
-        @childUpload="idbackimgFun"
+        @childUpload="accountfrontimgFun"
         v-model="accountItem.accountfrontimg"
-        label="身份证反面"
-      ></upload> -->
+        label="银行卡正面"
+      ></upload>
       <div class="btn-con" style="margin: 16px">
         <button class="btn pre-btn" @click="preTab">上一步</button>
         <button class="btn next-btn" type="submit">下一步</button>
@@ -38,6 +92,51 @@
           :columns="accounttypeList"
           @cancel="showPicker = false"
           @confirm="onConfirmPicker"
+        />
+      </van-popup>
+      <van-popup v-model="showPickerProvince" round position="bottom">
+        <van-picker
+          show-toolbar
+          value-key="provinceName"
+          :columns="provinceList"
+          @cancel="showPickerProvince = false"
+          @confirm="onConfirmPickerProvince"
+        />
+      </van-popup>
+      <van-popup v-model="showPickerCity" round position="bottom">
+        <van-picker
+          show-toolbar
+          value-key="cityName"
+          :columns="cityAccountList"
+          @cancel="showPickerCity = false"
+          @confirm="onConfirmPickerCity"
+        />
+      </van-popup>
+      <van-popup v-model="showPickerBank" round position="bottom">
+        <van-picker
+          show-toolbar
+          value-key="bankname"
+          :columns="bankList"
+          @cancel="showPickerBank = false"
+          @confirm="onConfirmPickerBank"
+        />
+      </van-popup>
+      <van-popup v-model="showPickerBranch" round position="bottom">
+        <van-picker
+          show-toolbar
+          value-key="branchName"
+          :columns="bankBranchList"
+          @cancel="showPickerBranch = false"
+          @confirm="onConfirmPickerBranch"
+        />
+      </van-popup>
+      <van-popup v-model="showPickerIdcardtype" round position="bottom">
+        <van-picker
+          show-toolbar
+          value-key="label"
+          :columns="idcardtypeAccountList"
+          @cancel="showPickerIdcardtype = false"
+          @confirm="onConfirmIdcardtype"
         />
       </van-popup>
     </template>
@@ -54,18 +153,28 @@ export default {
   data() {
     return {
       provinceList: [], // 开户省份
+      showPickerProvince: false,
       cityAccountList: [], // 开户城市
+      showPickerCity: false,
       bankList: [], //开户银行
+      showPickerBank: false,
       bankBranchList: [], // 开户银行支行
+      showPickerBranch: false,
+
       accountItem: {
         accountname: '', // 开户人姓名
         accounttype: '', //账户类型
         accounttypeP: '',
         province: '', //省份
+        provinceP: '',
         city: '', //城市
+        cityP: '',
         bankid: '', //开户银行
+        bankidP: '',
         branchid: '', //开户银行支行
+        branchidP: '',
         idcardtype: '', //	持卡人证件类型
+        idcardtypeP: '',
         idcard: '', //	证件编号
         bcvalidstartdate: '', //
         bcvalidenddate: '',
@@ -89,14 +198,53 @@ export default {
       ],
       showPicker: false,
       isaccounttype: false,
+      showPickerIdcardtype: false,
+      idcardtypeAccountList: [
+        { label: '身份证', value: '1' },
+        { label: '护照', value: '2' },
+        { label: '军人证', value: '3' },
+      ],
     }
   },
   methods: {
+    // 开户省份
+    onConfirmPickerProvince(item) {
+      this.accountItem.province = item.provinceCode
+      this.accountItem.provinceP = item.provinceName
+      this.showPickerProvince = false
+    },
+    // 开户城市
+    onConfirmPickerCity(item) {
+      this.accountItem.province = item.cityCode
+      this.accountItem.provinceP = item.cityName
+      this.showPickerCity = false
+    },
+    // 开户银行
+    onConfirmPickerBank(item) {
+      this.accountItem.bankid = item.cityCode
+      this.accountItem.bankidP = item.cityName
+      this.showPickerBank = false
+    },
+    // 开户银行支行
+    onConfirmPickerBranch(item) {
+      this.accountItem.branchid = item.branchId
+      this.accountItem.branchidP = item.branchName
+      this.showPickerBranch = false
+    },
+    // 证件类型
+    onConfirmIdcardtype(item) {
+      this.accountItem.idcardtype = item.value
+      this.accountItem.idcardtypeP = item.label
+      this.showPickerIdcardtype = false
+    },
     onConfirmPicker(item) {
       this.accountItem.accounttype = item.value
       this.accountItem.accounttypeP = item.label
       this.changeaccounttype(item.value)
       this.showPicker = false
+    },
+    accountfrontimgFun(val) {
+      this.accountItem.accountfrontimg = val
     },
     changeaccounttype(val) {
       switch (val) {
